08b450019242403d635d35d36e72dff4e21d9269
Samir Hasanov <samirhsn0104@gmail.com>
2023-09-16 13:34:15 +0400

    slide change

--- js/app.js
+++ js/app.js
@@ -17,10 +17,7 @@ AOS.init();
 let swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+    loop:true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
